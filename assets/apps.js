0689c2f1f30f5a19473df4c29db351ce292515d1
Takuto- <julien.verstraet31@gmail.com>
2020-12-10 11:35:22 +0100

    modif

--- assets/apps.js
+++ assets/apps.js
@@ -1,20 +1,34 @@
-let battletag = document.getElementById("battletag").value;
-console.log(battletag);
+let initBattleTag = document.getElementById("battletag").value;
+let battletag = initBattleTag.replace("#", "-");
 
+let initNom = document.getElementById("nom").value;
+let initPrenom = document.getElementById("prenom").value;
 
-function profile(plat, reg, tag){
+// function conforme(initNom , initPrenom) {
+//     for (initNom && initPrenom )
+// }
+
+function profile(plat, reg, tag) {
     let result = "https://ow-api.com/v1/stats/" + plat + "/" + reg + "/" + tag + "/profile";
     return result;
 }
 
+var reqHttp = new XMLHttpRequest(); // requête HTTP vers le serveurs
 
-console.log(profile("pc", "eu", battletag))
-const OverwatchAPI = () => {
-fetch(profile("pc", "eu", battletag))
-    .then(json => json.text())
-    .then(data => {
-        const profiles = JSON.parse(data);
+reqHttp.onreadystatechange = OverwatchAPI = () => { // instructions a suivre pour traiter les infos reçues (réponse du serveur)
+
+    
+    if (reqHttp.readyState === XMLHttpRequest.DONE) {// si j'ai une réponse du serveur faire ceci
+        console.log(reqHttp.responseText)
+        const profile = JSON.parse(reqHttp.responseText); // passer au format JSON
+
+        console.log(profile["name"], profile); // console log pour récuperer le battletag
 
 
-});
-}
+
+    }
+
+};
+reqHttp.open("GET", profile("pc", "eu", "Tyra-22283"), true);
+reqHttp.send();
+
