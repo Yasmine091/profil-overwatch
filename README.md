24c5b6c34e68fdd7ca3e820d30f12f69e09186c8
Yasmine091 <yasmine.emdounan@gmail.com>
2020-12-14 15:26:33 +0100

    modifs de "sécurité" (ajout de conditons)

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 La doc de l'API qu'on va utiliser : https://ow-api.com/docs/#introduction
 
 
-* Créer un formulaire permettant de saisir date de naissance, nom, prénom et battletag. [FAIT]
+* Créer un formulaire permettant de saisir date de naissance, nom, prénom et battletag.
 
-* Grâce à JS, vérifiez dès qu'on change d'input que le champ est conforme à ce qui est attendu. [FAIT]
+* Grâce à JS, vérifiez dès qu'on change d'input que le champ est conforme à ce qui est attendu.
 
-* En particulier, ajoutez un validateur custom pour vérifier le format du battletag. [FAIT]
+* En particulier, ajoutez un validateur custom pour vérifier le format du battletag.
 
-* Directement dans l'input, remplacer # par -. [FAIT]
+* Directement dans l'input, remplacer # par -.
 
 * Si le battletag est au bon format, utilisez l'API indiquée plus haut (via AJAX) pour récupérer les infos du profil et affichez les infos suivantes directement le DOM grâce à JS :
 
--- assets/apps.js
+++ assets/apps.js
@@ -1,71 +1,119 @@
 document.getElementById("profile").style.display = "none";
+let nom = document.getElementById("nom");
+let prenom = document.getElementById("prenom");
+let bTag = document.getElementById("battletag");
+
+let error = document.getElementById("error");
+error.style.display = "none";
+
+let button = document.getElementById("send");
+button.disabled = true; 
+
+
+// vérifier que tous les champs sont bons
+function checkForm() {
+
+    let isCorrect = "2px solid green";
+    
+    // si tout est bon activer le bouton
+    if(nom.style.border == isCorrect && prenom.style.border == isCorrect && bTag.style.border == isCorrect){
+        button.disabled = false;
+    }
+    
+    else { // si qlq chose ne va pas
+        button.disabled = true;
+    }
+}
+
 
 function checkThis(champ) {
 
     let input = champ;
     let domatch = /^[a-zA-Z]+$/.test(input.value);
+    let inpVal = input.value;
 
-    if (input.value != "" && domatch == true) {
+    if (input.value != "" && domatch == true && inpVal.length >= 3 && inpVal.length <= 12) {
         input.style.border = "2px solid green";
+        error.style.display = "none";
+        return true;
     }
     else {
         input.style.border = "2px solid red";
+        error.style.display = "block";
+        error.innerHTML = "Erreur, le " + input.name + " doit comprendre entre 3 et 12 caractères, et ne peut contenir que des lèttres.";
+        return false;
     }
 
 }
 
 function checkTag(battletag) {
+
     let tag = battletag;
+
     let match = /^[a-zA-Z0-9#]+$/.test(tag.value);
     let valid = tag.value.includes("#");
 
-    if (tag.value != "" && match == true && valid == true) {
+    let tagVal = tag.value;
+
+    if (tag.value != "" && match == true && valid == true && tagVal.length >= 3 && tagVal.length <= 12) {
         tag.style.border = "2px solid green";
+        error.style.display = "none";
+        return true;
     }
     else {
         tag.style.border = "2px solid red";
+        error.style.display = "block";
+        error.innerHTML = "Erreur, le " + tag.name + " doit comprendre entre 3 et 12 caractères, des lèttres, des chiffres et un #.";
+        return false;
     }
 
 
 }
 
-
 function profile(plat, reg, tag) {
-    let result = "https://ow-api.com/v1/stats/" + plat + "/" + reg + "/" + tag + "/profile";
+    let result =
+    "https://ow-api.com/v1/stats/" + plat + "/" + reg + "/" + tag + "/complete";
     return result;
 }
 
 const OverwatchAPI = () => {
 
-    document.getElementById("formulaire").style.display = "none";
-    document.getElementById("profile").style.display = "block";
-
+    error.style.display = "none";
     var reqHttp = new XMLHttpRequest(); // requête HTTP vers le serveurs
-
+    
     reqHttp.onreadystatechange = function () { // instructions a suivre pour traiter les infos reçues (réponse du serveur)
+        
+        
+        if (reqHttp.readyState === XMLHttpRequest.DONE && reqHttp.status == 200) {// si j'ai une réponse du serveur faire ceci
+        
+        document.getElementById("formulaire").style.display = "none";
+        document.getElementById("profile").style.display = "block";
 
-
-        if (reqHttp.readyState === XMLHttpRequest.DONE) {// si j'ai une réponse du serveur faire ceci
-
-            const profile = JSON.parse(reqHttp.responseText); // passer au format JSON
+            const profile = JSON.parse(reqHttp.responseText);
 
             const ul = document.getElementById("stats");
 
             function showContent(content) {
+                
                 let li = document.createElement("li");
                 li.style.listStyle = "none";
                 li.innerHTML = content;
                 ul.appendChild(li);
+                
             }
 
             showContent("<h1>" + profile["name"] + "</h1>");
             showContent("<h2>Statistiques</h2>");
-            showContent("<b>Parties jouées : </b>" + profile["competitiveStats"]["games"]["played"]);
-            showContent("<b>Classement : </b>" + profile["level"]);
-            showContent("<b>Nombre de victoires : </b>" + profile["gamesWon"]);
-            showContent("<b>Nombre de défaites : </b>" + profile["competitiveStats"]["games"]["played"]);
+            showContent("<b>Parties jouées : </b>" + profile["quickPlayStats"]["careerStats"]["allHeroes"]["game"]["gamesPlayed"]);
+            showContent("<b>Classement : </b>" + profile["rating"]);
+            showContent("<b>Nombre de victoires : </b>" + profile["quickPlayStats"]["careerStats"]["allHeroes"]["game"]["gamesWon"]);
+            showContent("<b>Nombre de défaites : </b>" + profile["quickPlayStats"]["careerStats"]["allHeroes"]["game"]["gamesLost"]);
 
 
+        }
+        else {
+            error.style.display = "block";
+            error.innerHTML = "Erreur, ce Battletag n'existe pas.";
         }
 
     };
